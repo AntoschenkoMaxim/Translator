93d53b471880db11bf1769648463f089d6f982df
Maxim <jurusski@gmail.com>
2022-09-04 00:35:28 +0300

    Creating "PageRouting" page
    Created "PageRouting" page.

--- translator/src/components/PageComponents/PageRouting/PageRouting.jsx
+++ translator/src/components/PageComponents/PageRouting/PageRouting.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import Card from '../../UIComponents/Card/Card'
+import classes from './PageRouting.module.css'
+import { MdOutlineGTranslate, MdStarOutline } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
 
 const PageRouting = () => {
+
+	const navigate = useNavigate()
+
 	return (
-		<Card>fssdfsdf</Card>
+		<div className={classes.container}>
+			<Card>
+				<div onClick={() => navigate('/translator')} className={classes.card__content}>
+					<div><MdOutlineGTranslate className={classes.card__icon} /></div>
+					<div className={classes.card__text}>Translate</div>
+				</div>
+			</Card>
+			<Card>
+				<div onClick={() => navigate('/favourites')} className={classes.card__content}>
+					<div><MdStarOutline className={classes.card__icon} /></div>
+					<div className={classes.card__text}>Favourites</div>
+				</div>
+			</Card>
+		</div>
+
 	)
 }
 
