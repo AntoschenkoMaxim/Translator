9f6d7abf0637608263b6036ade72d1102f3693f4
Maxim <jurusski@gmail.com>
2022-09-10 00:14:49 +0300

    Creating async function "translation"
    Created async function "translation"

--- translator/src/API/TranslatorService.js
+++ translator/src/API/TranslatorService.js
@@ -22,4 +22,16 @@ export default class TranslatorService {
 		})
 		return response
 	}
+
+	static async translation() {
+		const response = await axios.post('https://deep-translate1.p.rapidapi.com/language/translate/v2', {
+			headers: {
+				'content-type': 'application/json',
+				'X-RapidAPI-Key': 'a0e3f8f0b9mshf3cfa708809408dp140f77jsnfcfcd331a786',
+				'X-RapidAPI-Host': 'deep-translate1.p.rapidapi.com'
+			},
+			data: '{"q":"Hello World!","source":"en","target":"es"}'
+		})
+		return response
+	}
 }
