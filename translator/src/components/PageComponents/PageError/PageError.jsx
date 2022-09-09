77a7f288fa69b3553f76284f6956010359ae4786
Maxim <jurusski@gmail.com>
2022-09-10 00:29:07 +0300

    Creating page "PageError" with styles
    Created page "PageError" with styles

--- translator/src/components/PageComponents/PageError/PageError.jsx
+++ translator/src/components/PageComponents/PageError/PageError.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import classes from './PageError.module.css'
+import { Link } from 'react-router-dom'
+import img from '../../../images/not-found.svg'
 
 const PageError = () => {
 	return (
-		<div>PageError</div>
+		<div className={classes.error}>
+			<img src={img} alt='not found' className={classes.error__image} />
+			<h3 className={classes.error__title}>Ohh! Page not found!</h3>
+			<p className={classes.error__subtitle}>This page could not be found.</p>
+			<Link to='/' className={classes.error__link}>Back</Link>
+		</div>
 	)
 }
 
