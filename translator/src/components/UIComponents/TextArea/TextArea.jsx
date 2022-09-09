4a30cd16a094b0e64571b7f29b734bbd8aa5c100
Maxim <jurusski@gmail.com>
2022-09-10 00:32:33 +0300

    Creating UI component "Textarea" with styles
    Created UI component "Textarea" with styles.

--- translator/src/components/UIComponents/TextArea/TextArea.jsx
+++ translator/src/components/UIComponents/TextArea/TextArea.jsx
@@ -1,13 +1,45 @@
 import React from 'react'
 import classes from './TextArea.module.css'
+import Select from 'react-select'
 
-const TextArea = (props) => {
+const TextArea = ({
+	selectedLanguage,
+	setSelectedLanguage,
+	languages,
+	style,
+	textForTranslation,
+	setTextForTranslation,
+	translatedText,
+	setTranslatedText,
+	detection,
+	setDetection,
+	...props }) => {
+
+	const handleClick = () => {
+		setTextForTranslation()
+		setTranslatedText()
+	}
+
+	const handleChange = (e) => {
+		setTextForTranslation(e.target.value)
+	}
+
 	return (
-		<textarea
-			type='text'
-			className={classes.textarea}
-			{...props}
-		/>
+		<div className={style}>
+			<Select value={selectedLanguage} onChange={setSelectedLanguage} options={languages} />
+			<textarea
+				placeholder={style === 'inputTextarea' ? 'Writing text...' : 'Translation'}
+				type='text'
+				className={classes.textarea}
+				disabled={style === classes.outputTextarea}
+				value={style === 'input' ? textForTranslation : translatedText}
+				onChange={handleChange}
+				{...props}
+			/>
+			{style === 'input' && (
+				<div className={classes.delete} onClick={handleClick}></div>
+			)}
+		</div>
 	)
 }
 
