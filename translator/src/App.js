20923961aea8e8d774cb169b365213a9afc0046c
Maxim <jurusski@gmail.com>
2022-09-27 00:51:53 +0300

    Changing routes
    Routes changed

--- translator/src/App.js
+++ translator/src/App.js
@@ -2,18 +2,14 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Error from './pages/Error';
 import Favourites from './pages/Favourites';
-import FavouritesId from './pages/FavouritesId';
-import Routing from './pages/Routing';
 import Translator from './pages/Translator';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Routing />} />
-        <Route path='translator' element={<Translator />} />
+        <Route index element={<Translator />} />
         <Route path='favourites' element={<Favourites />} />
-        <Route path='favourites/:id' element={<FavouritesId />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
