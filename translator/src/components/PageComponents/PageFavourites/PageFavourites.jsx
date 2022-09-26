b1f2b47276743fab4d6004620d850e3b6e689e32
Maxim <jurusski@gmail.com>
2022-09-27 00:57:02 +0300

    Creating base structure "PageFavourites" with styles
    Created base structure "PageFavourites" with styles

--- translator/src/components/PageComponents/PageFavourites/PageFavourites.jsx
+++ translator/src/components/PageComponents/PageFavourites/PageFavourites.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+import FavouriteList from '../../CompositeComponents/FavouriteList/FavouriteList'
+import TranslationFilter from '../../CompositeComponents/TranslationFilter/TranslationFilter'
+import { useItems } from '../../hooks/useItems'
+import { useLocalStorageCustom } from '../../hooks/useLocalStorage'
+import useLocalStorage from 'use-local-storage'
+import classes from './PageFavourites.module.css'
+import { BsLightningChargeFill, BsLightningCharge } from 'react-icons/bs'
 
 const PageFavourites = () => {
+
+	}
+
 	return (
-		<div>PageFavourites</div>
+		<div className='page' data-theme={theme}>
+			<div className={classes.theme} onClick={switchTheme}>{theme === 'dark' ? <BsLightningCharge className={classes.theme__icon} /> : <BsLightningChargeFill className={classes.theme__icon} />}</div>
+			<TranslationFilter filter={filter} setFilter={setFilter} title='Filters' button='Translator' path='/' />
+			<FavouriteList favouriteItems={sortedAndSearchedItems} title='Favourite history' />
+		</div>
 	)
 }
 
