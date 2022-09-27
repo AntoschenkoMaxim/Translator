3d6ab554ee378e5d7bc3f3853e99c7ad6572f313
Maxim <jurusski@gmail.com>
2022-09-27 17:19:49 +0300

    Removing unused functions
    Removed unused functions

--- translator/src/components/hooks/useDebounced.js
+++ translator/src/components/hooks/useDebounced.js
@@ -3,12 +3,11 @@ import { useCallback, useRef } from "react";
 export default function useDebounce(callback, delay) {
 	const timer = useRef()
 
-	// функцию передаем только в том случае если callback или задержка изменились
 	const debouncedCallback = useCallback((...args) => {
 		if (timer.current) {
 			clearTimeout(timer.current)
 		}
-		// делаем таймаут и если данная функция вызвалась еще раз, старый таймаут удаляем и задаем новый, таким образом перезаписываем таймаут
+
 		timer.current = setTimeout(() => {
 			callback(...args)
 		}, delay)
